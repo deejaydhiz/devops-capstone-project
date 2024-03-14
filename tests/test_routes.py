58090e1220dcc44e5dd1ae0c0cdda1448aa6eb72
Deji Banire <125389998+deejaydhiz@users.noreply.github.com>
2024-03-14 08:23:15 -0400

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,3 +138,9 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)   
+        
+    def test_update_account(self):
+        """It should update a single account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.post(
+      
